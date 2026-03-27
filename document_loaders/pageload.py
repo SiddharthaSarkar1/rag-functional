a5c6239e736744d54962353426c17e5281d30bd1
SiddharthaSarkar1 <mail.04.siddhartha@gmail.com>
2026-03-27 21:15:09 +0530

    load data using PDF loader and Web based loader

---
+++ document_loaders/pageload.py
@@ -0,0 +1,11 @@
+from langchain_community.document_loaders import WebBaseLoader
+
+url = "https://www.apple.com/in/macbook-neo/"
+
+data = WebBaseLoader(url)
+
+docs = data.load()
+
+print(len(docs))
+print(docs)
+print(docs[0].page_content)
